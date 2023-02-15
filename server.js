19638b7f0b7bf059dceea641bab21a0635a6ae5d
indra494 <indra494@gmail.com>
2023-02-16 01:07:50 +0900

    상품기능추가

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const cors = require('cors');
 const app = express();
 const models = require('./models');
 const { json } = require('sequelize');
+const multer = require('multer');
+const upload = multer({
+    storage: multer.diskStorage({
+        destination : function(req, file, cb) {
+            cb(null, 'uploads/');
+        },
+        filename: function(req,file,cb) {
+            cb(null, file.originalname);
+        }
+    })
+})
 const port = 8080;
 
 app.use(express.json());
 app.use(cors());
+app.use('/uploads', express.static('uploads'))
 
 app.get('/products',(req,res) => {
 
@@ -20,7 +32,7 @@ app.get('/products',(req,res) => {
         })
     }).catch((error)=>{
         console.log(error);
-        res.send("에러발생");
+        res.status(400).send("에러발생");
     });
 });
 
@@ -39,7 +51,7 @@ app.get('/products/:id', (req,res) => {
         });
     }).catch((error)=> {
         console.log(error);
-        res.send("상품 조회에 에러가 발생하였습니다.");
+        res.status(400).send("상품 조회에 에러가 발생하였습니다.");
     });
 
 });
@@ -47,21 +59,22 @@ app.get('/products/:id', (req,res) => {
 
 app.post('/products',(req,res) => {
     const body = req.body;
-    const {name, description, price, seller} = body;
+    const {name, description, price, seller, imageUrl} = body;
 
     console.log(name);
     console.log(description);    
     console.log(price);    
     console.log(seller);       
 
-    if(!name || !description || !price || !seller) {
-        res.send('모든 필드를 입력해주세요');
+    if(!name || !description || !price || !seller || !imageUrl) {
+        res.status(400).send('모든 필드를 입력해주세요');
     }
     models.product.create({
         name,
         description,
         price,
         seller,
+        imageUrl
     }).then((result)=>{
         console.log("상품 생성결과",result);
         res.send({
@@ -69,8 +82,15 @@ app.post('/products',(req,res) => {
         });
     }).catch((error)=>{
         console.log(error);
-        res.send('상품 업로드에 문제가 발생하였습니다.')
+        res.status(400).send('상품 업로드에 문제가 발생하였습니다.')
     });
+});
+
+app.post('/image',upload.single('image'),(req,res) => {
+    const file = req.file;
+    res.send({
+        imageUrl : file.path
+    })
 });
 
 app.listen(port, () => {
